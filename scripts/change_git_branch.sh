2a64b3f9ba6181ed56f6d4beabf28b90f215d917
Jesse Stilwell <jesse@stilwell.io>
2022-09-01 14:38:30 -0400

    Add change git branch playbook and script

---
+++ scripts/change_git_branch.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+# Script logic begin
+
+codedir=$1
+gitbranch=$2
+gitpass=$3
+filename="dirlist.txt"
+
+# Answer propmt for git password one time only.
+echo "$gitpass" | ssh-add /root/.ssh/id_rsa.rlgit
+    
+# Get list of subdirectories with .git folders (external plugins)
+find $codedir -name ".git" -type d > $filename
+
+while read line; do
+# Change directory and move one down. 
+cd $line && cd ..
+# Checkout branch specified by user.
+git fetch origin && git checkout -b $gitbranch origin/$gitbranch
+done < $filename
